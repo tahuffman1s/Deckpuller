779ab95024c07cc3f6b4f292afafab6dcec931ba
tahuffman1s <huffmantravis57@protonmail.com>
2026-06-10 17:22:59 -0500

    build: add navigation-compose and datastore-preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
 
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
